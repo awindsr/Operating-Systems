6a13a762e0b873e0a0c9c3f5c3dadcc0ea6b072c
Awindsr <84223604+awindsr@users.noreply.github.com>
2024-04-08 12:09:41 +0530

    Update roundrobin.c

--- roundrobin.c
+++ roundrobin.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int length, totalTime = 0, count = 0, remainingProcessCount = 0;
-    float avgWaitTime = 0;
-    int processID[length], burstTime[length], waitTime[length], turnAroundTime[length], timeQuant = 0;
-
+    int length, totalTime = 0, count = 0;
+    float avgWaitTime = 0, totalWaitTime=0, avgTurnAroundTime;
+    
     printf("Enter the total number of processes: ");
     scanf("%d", &length);
+    int processID[length], burstTime[length], waitTime[length], turnAroundTime[length], timeQuant = 0, remainingTime[length], remainingProcessCount = length;
 
     for (int i = 0; i < length; i++) {
         printf("Enter the burst time of process %d: ", i);
@@ -16,23 +16,27 @@ int main() {
 
     printf("Enter the time quantum: ");
     scanf("%d", &timeQuant);
-    turnAroundTime[0]=burstTime[0];
-
+    turnAroundTime[0] = burstTime[0];
+    
     for (int i = 0; i < length; i++) {
-        if (burstTime[i] <= timeQuant && burstTime[i] > 0) {
-            totalTime += burstTime[i];
-            burstTime[i] = 0;
-            count++;
-        } else if (burstTime[i] > timeQuant) {
-            burstTime[i] -= timeQuant;
-            totalTime += timeQuant;
-        }
+        remainingTime[i] = burstTime[i];
     }
 
-    for (int i = 1; i < length; i++) {
-        waitTime[i] = totalTime - burstTime[i];
-        turnAroundTime[i] = turnAroundTime[i-1] + burstTime[i];
-        avgWaitTime += waitTime[i];
+    while (remainingProcessCount > 0) {
+        for (int i = 0; i < length; i++) {
+            if (remainingTime[i] <= timeQuant && remainingTime[i] > 0) {
+                totalTime += remainingTime[i];
+                turnAroundTime[i] = totalTime;
+                remainingTime[i] = 0;
+                remainingProcessCount--;
+		waitTime[i] = totalTime - burstTime[i];
+		totalWaitTime += waitTime[i];
+            }
+            else if (remainingTime[i] > 0) {
+                remainingTime[i] -= timeQuant;
+                totalTime += timeQuant;
+            }
+        }
     }
     
     printf("Process ID\tBurst Time\tWait Time\tTurnaround Time\n");
@@ -40,10 +44,12 @@ int main() {
         printf("%d\t\t%d\t\t%d\t\t%d\n", processID[i], burstTime[i], waitTime[i], turnAroundTime[i]);
     	}
 
-    avgWaitTime /= length;
+ 
+    avgWaitTime = (float)totalWaitTime / length;
+    avgTurnAroundTime = (float)totalTime / length;
 
-    printf("Average waiting time: %f\n", avgWaitTime);
+    printf("Total wait time is %f\nAverage wait time is %f\n", totalWaitTime, avgWaitTime);
+    printf("Total Turnaround time is %d\nAverage turnaround time is %f\n", totalTime, avgTurnAroundTime);
 
     return 0;
 }
-
